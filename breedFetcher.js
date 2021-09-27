1398511f8bdabbba065f540c41a68ca667dc2a91
AJ <aj@smartnvm.com>
2021-09-27 00:39:15 +0000

    export module and add error checking for no breed query result

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,36 +5,40 @@ const request = require('request');
 
 
 
-const breedFecher = function(query) {
+const fetchBreedDescription = function (query, callback) {
   // use request to fetch IP address from JSON API
-  
+
   let domain = ` https://api.thecatapi.com/v1/breeds/search?q=${query}`;
 
   //console.log(query)
   request(domain, (error, response, body) => {
 
-    if (error) return  'erro';//callback(`\n ERROR: ${error.message}`, null, null);
+    if (error) return callback(`\n ERROR: ${error.message}`, null);
 
     if (response.statusCode !== 200) {
       const errMsg = console.log(`Error: ${response && response.statusCode} while fetching URL: ${domain}`);
-      return errMsg;
+      return callback(errMsg, null);
     }
 
-    
-    
+
+
     let data = JSON.parse(body);
 
-    
+
     //console.log(data)
     console.log('\nquery:', query);
     console.log('-------------------');
-    if (data.length === 0) return console.log('Error: breed not found');
-    console.log(data[0].description);
-   
+    let err = 'Error: breed not found'
+
+    if (data.length === 0) return callback(err, null)
+    
+    return callback(null, data[0].description);
+
   });
 
 };
 
-let query = process.argv.slice(2);
 
-breedFecher(`${query}`);
+
+module.exports = { fetchBreedDescription };
+
